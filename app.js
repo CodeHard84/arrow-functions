8be7616e55fc025fc2bb9d6437c876cef397c673
Jace <sockgnome@att.net>
2024-04-17 01:56:26 +0000

    Completed step 11

--- app.js
+++ app.js
@@ -204,17 +204,18 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scope();
+joe.scope();
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scopeArrow();
+joe.scopeArrow();
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+// this refers to this student or in our case Joe.
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// It looks like in this case 'this' is referring to the window.
 // 3. Explain why "this" is different when an arrow function is used.
-//
+// From what I have gathered it appears the arrow function takes the scope from it's surrounding code/context where
+// the non-arrow function takes it context from the constructor and function.
